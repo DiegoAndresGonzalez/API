8f37eceb4ed87229c308bbd2a40bed083f2b575c
Juan Pablo <fonseca123papo@gmail.com>
2023-06-21 09:06:23 -0500

    creando el metodo createRestaurant en la interface IRestaurantServicePort

--- src/main/java/com/pragma/app/domain/api/IRestaurantServicePort.java
+++ src/main/java/com/pragma/app/domain/api/IRestaurantServicePort.java
@@ -1,4 +1,8 @@
 package com.pragma.app.domain.api;
 
+import com.pragma.app.domain.model.RestaurantModel;
+
 public interface IRestaurantServicePort {
+
+    void createRestaurant(RestaurantModel restaurantModel);
 }
