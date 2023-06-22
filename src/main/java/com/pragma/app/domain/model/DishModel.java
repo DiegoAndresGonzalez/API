29847a744089fff99ee0a081c98e937f5171d489
Diego <deworky@gmail.com>
2023-06-21 23:05:32 -0500

    Añadiendo DishModel

---
+++ src/main/java/com/pragma/app/domain/model/DishModel.java
@@ -0,0 +1,81 @@
+package com.pragma.app.domain.model;
+
+public class DishModel {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String urlImage;
+    private String category;
+    private Long restaurantId;
+    private boolean active;
+
+    public DishModel(Long id, String name, String description, String urlImage, String category, Long restaurantId, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.urlImage = urlImage;
+        this.category = category;
+        this.restaurantId = restaurantId;
+        this.active = active;
+    }
+
+    public DishModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Long getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(Long restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
