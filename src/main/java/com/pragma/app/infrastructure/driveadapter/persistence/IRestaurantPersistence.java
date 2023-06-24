0bbcf859ffd16842febc45229e2cb4810db8c1f2
Juan Pablo <fonseca123papo@gmail.com>
2023-06-24 12:15:19 -0500

    haciendo la clase RestaurantMapper

--- src/main/java/com/pragma/app/infrastructure/driveadapter/persistence/IRestaurantPersistence.java
+++ src/main/java/com/pragma/app/infrastructure/driveadapter/persistence/IRestaurantPersistence.java
@@ -1,8 +1,10 @@
 package com.pragma.app.infrastructure.driveadapter.persistence;
 
 import com.pragma.app.domain.model.RestaurantModel;
+import com.pragma.app.infrastructure.driveadapter.entity.RestaurantEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IRestaurantPersistence {
+public interface IRestaurantPersistence extends JpaRepository<RestaurantEntity, Long> {
 
     RestaurantModel saveRestaurantPersistence(RestaurantModel restaurantModel);
 }
--- src/main/java/com/pragma/app/infrastructure/entrypoint/RestaurantController.java
+++ src/main/java/com/pragma/app/infrastructure/entrypoint/RestaurantController.java
@@ -1,4 +1,27 @@
 package com.pragma.app.infrastructure.entrypoint;
 
+import com.pragma.app.domain.model.RestaurantModel;
+import com.pragma.app.infrastructure.driveadapter.persistence.IRestaurantPersistence;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurants")
 public class RestaurantController {
+
+    private final IRestaurantPersistence restaurantPersistence;
+
+    public RestaurantController(IRestaurantPersistence restaurantPersistence) {
+        this.restaurantPersistence = restaurantPersistence;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveNewRestaurant(@RequestBody RestaurantModel restaurantModel){
+        restaurantPersistence.saveRestaurantPersistence(restaurantModel);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
