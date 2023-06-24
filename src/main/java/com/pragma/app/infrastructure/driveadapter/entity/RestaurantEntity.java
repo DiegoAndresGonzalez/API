c85a354bbba34d7f56eeb32cced994e3336c83bb
Juan Pablo <fonseca123papo@gmail.com>
2023-06-24 11:27:59 -0500

    modificando la clase RestaurantEntity

--- src/main/java/com/pragma/app/infrastructure/driveadapter/entity/RestaurantEntity.java
+++ src/main/java/com/pragma/app/infrastructure/driveadapter/entity/RestaurantEntity.java
@@ -6,21 +6,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
-
+@Entity
+@Data
 public class RestaurantEntity {
 
-    @Entity
-    @Data
-    public class RestaurantModel {
-
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long idRestaurant;
-        private String name;
-        private String nit;
-        private String address;
-        private String cellPhoneRestaurant;
-        private String urlLogo;
-        private Long idOwner;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRestaurant;
+    private String name;
+    private String nit;
+    private String address;
+    private String cellPhoneRestaurant;
+    private String urlLogo;
+    private Long idOwner;
 }
