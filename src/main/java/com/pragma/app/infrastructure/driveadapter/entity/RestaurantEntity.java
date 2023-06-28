abe0f23a292fcb5383aebcbcba6b262c8508700a
Juan Pablo <fonseca123papo@gmail.com>
2023-06-28 06:53:45 -0500

    modificando la clase RestaurantEntity

--- src/main/java/com/pragma/app/infrastructure/driveadapter/entity/RestaurantEntity.java
+++ src/main/java/com/pragma/app/infrastructure/driveadapter/entity/RestaurantEntity.java
@@ -1,13 +1,14 @@
 package com.pragma.app.infrastructure.driveadapter.entity;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-import lombok.Data;
+@Table(name = "restaurant")
 @Entity
-@Data
 public class RestaurantEntity {
 
     @Id
@@ -19,4 +20,73 @@ public class RestaurantEntity {
     private String cellPhoneRestaurant;
     private String urlLogo;
     private Long idOwner;
+
+    public RestaurantEntity() {
+    }
+
+    public RestaurantEntity(Long idRestaurant, String name, String nit, String address, String cellPhoneRestaurant, String urlLogo, Long idOwner) {
+        this.idRestaurant = idRestaurant;
+        this.name = name;
+        this.nit = nit;
+        this.address = address;
+        this.cellPhoneRestaurant = cellPhoneRestaurant;
+        this.urlLogo = urlLogo;
+        this.idOwner = idOwner;
+    }
+
+    public Long getIdRestaurant() {
+        return idRestaurant;
+    }
+
+    public void setIdRestaurant(Long idRestaurant) {
+        this.idRestaurant = idRestaurant;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCellPhoneRestaurant() {
+        return cellPhoneRestaurant;
+    }
+
+    public void setCellPhoneRestaurant(String cellPhoneRestaurant) {
+        this.cellPhoneRestaurant = cellPhoneRestaurant;
+    }
+
+    public String getUrlLogo() {
+        return urlLogo;
+    }
+
+    public void setUrlLogo(String urlLogo) {
+        this.urlLogo = urlLogo;
+    }
+
+    public Long getIdOwner() {
+        return idOwner;
+    }
+
+    public void setIdOwner(Long idOwner) {
+        this.idOwner = idOwner;
+    }
 }
