cd57a864f515469be0689221f8ef40511a82cf86
Juan Pablo <fonseca123papo@gmail.com>
2023-06-28 08:37:04 -0500

    modificando el codigo fuente del controller admin

--- src/main/java/com/pragma/app/infrastructure/entrypoint/AdminController.java
+++ src/main/java/com/pragma/app/infrastructure/entrypoint/AdminController.java
@@ -4,6 +4,9 @@ import com.pragma.app.domain.model.RestaurantModel;
 import com.pragma.app.domain.spi.IRestaurantPersistencePort;
 import com.pragma.app.infrastructure.driveadapter.entity.RestaurantEntity;
 import com.pragma.app.infrastructure.driveadapter.mapper.IRestaurantMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +27,15 @@ public class AdminController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> saveRestaurant(@RequestBody RestaurantEntity restaurantEntity){
+    @Operation(summary = "Add a new Restaurant")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Restaurant created"),
+            @ApiResponse(responseCode = "400", description = "The format in the fields is invalid"),
+            @ApiResponse(responseCode = "403", description = "Role other than admin"),
+            @ApiResponse(responseCode = "404", description = "No user found with that id"),
+            @ApiResponse(responseCode = "409", description = "Conflict")
+    })
+    public ResponseEntity<String> saveRestaurant(@RequestBody RestaurantEntity restaurantEntity){
         RestaurantModel restaurantModel = restaurantMapper.mapToRestaurantModel(restaurantEntity);
         restaurantPersistencePort.saveRestaurantPersistence(restaurantModel);
         return ResponseEntity.status(HttpStatus.CREATED).build();
