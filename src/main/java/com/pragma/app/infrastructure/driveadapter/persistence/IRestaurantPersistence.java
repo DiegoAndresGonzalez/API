86042a73fa0742c1cf329a88bb25a7aa9a7c07f2
Juan Pablo <fonseca123papo@gmail.com>
2023-06-28 06:36:11 -0500

    modificando properties

--- src/main/java/com/pragma/app/infrastructure/driveadapter/persistence/IRestaurantPersistence.java
+++ src/main/java/com/pragma/app/infrastructure/driveadapter/persistence/IRestaurantPersistence.java
@@ -2,7 +2,6 @@ package com.pragma.app.infrastructure.driveadapter.persistence;
 
 
 import com.pragma.app.infrastructure.driveadapter.entity.RestaurantEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IRestaurantPersistence {
 
--- src/main/java/com/pragma/app/infrastructure/driveadapter/repository/IRestaurantRepository.java
+++ src/main/java/com/pragma/app/infrastructure/driveadapter/repository/IRestaurantRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IRestaurantRepository extends JpaRepository<RestaurantEntity, Long> {
-
 }
