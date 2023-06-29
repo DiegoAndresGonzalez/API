0fe8e3ac0b6bce04f0f1b1b1b012bba1795788a3
Juan Pablo <fonseca123papo@gmail.com>
2023-06-29 07:00:14 -0500

    modificando nombres y clases DTO

--- src/main/java/com/pragma/app/application/dto/response/RestaurantResponseDTO.java
+++ src/main/java/com/pragma/app/application/dto/response/RestaurantResponseDTO.java
@@ -1,27 +1,16 @@
-package com.pragma.app.infrastructure.driveadapter.entity;
-
+package com.pragma.app.application.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "restaurant")
-@Entity
-public class RestaurantEntity {
+public class RestaurantResponseDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idRestaurant;
     private String name;
     private String nit;
